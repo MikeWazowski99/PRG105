ef1dde2a6ac358611e55c88390b2732466de32ba
Michael Tapia <89033407+MikeWazowski99@users.noreply.github.com>
2021-08-18 14:24:17 -0500

    Create Printing.py

---
+++ Printing.py
@@ -0,0 +1,16 @@
+'''
+
+Hola world
+'''
+
+print ('Now that shes back')
+print ('With drops of jupiter in her hair')
+print ('She acts like the star of a room')
+print ('Reminds me that theres still time to change')
+print ('Since the return of her stay on the moon')
+print ('Her voice as nice as the spring air')
+
+print ('But tell me did you sail across the sun')
+print ('Did you make it to the Milky Way to see all the lights')
+print ('Did you fall from a shooting star')
+print ('And did you miss me while you were out there looking for yourself')
