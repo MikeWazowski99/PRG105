d2ed3cba52e06267f3ba4db17eae156a4e1259c1
Michael Tapia <89033407+MikeWazowski99@users.noreply.github.com>
2021-09-20 13:17:10 -0500

    Add files via upload

---
+++ 4.2 Song.py
@@ -0,0 +1,11 @@
+# 4.2 Song Homework
+
+
+num = int(input("How many bottles are there on the wall? "))
+
+while num > 0:
+    print(num, "Bottles of beer on the wall")
+    print(num, "bottles of beer")
+    print("Take one down, pass it around")
+    num = num - 1
+    print(num, "bottles of beer on the wall")
