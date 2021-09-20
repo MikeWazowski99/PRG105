595cc5c968bfec4bfb04b2bced7b61f1e8ea496c
Michael Tapia <89033407+MikeWazowski99@users.noreply.github.com>
2021-09-20 13:59:56 -0500

    Add files via upload

--- 4.2 Song.py
+++ 4.2 Song.py
@@ -1,5 +1,5 @@
 # 4.2 Song Homework
-
+# Make a song that will take a bottle down until it reaches 0 
 
 num = int(input("How many bottles are there on the wall? "))
 
@@ -8,4 +8,4 @@ while num > 0:
     print(num, "bottles of beer")
     print("Take one down, pass it around")
     num = num - 1
-    print(num, "bottles of beer on the wall")
+    print(num, "bottles of beer on the wall\n\n")
