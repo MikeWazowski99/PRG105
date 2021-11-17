3fc5d6a3e34c626358792a8ed16d0c3fdb3ffcb5
Michael Tapia <89033407+MikeWazowski99@users.noreply.github.com>
2021-11-17 13:48:38 -0600

    Add files via upload

--- 12.1 CRUD.py
+++ 12.1 CRUD.py
@@ -18,18 +18,19 @@ Exit = 5
 
 def main():
     try:
-        input_file = open('customer_data', 'w')
+        input_file = open('customer_file.dat', 'rb')
         customers = pickle.load(input_file)
         print(customers)
+        input_file.close()
     except (FileNotFoundError, IOError):
         print('File not found, please add a customer then quit to create the file')
         customers = {}
 
     choice = 0
 
-    while choice != Exit:
-        choice = menu()
-        print(choice)
+    # while choice != Exit:
+    choice = menu()
+    print(choice)
 
     if choice == Look_up:
         look_up(customers)
@@ -65,11 +66,13 @@ def look_up(customers):
 
 def add(customers):
     name = input("Enter a customer's name: ")
-    email = name = input("Enter a customer's email: ")
+    email = input("Enter a customer's email: ")
     if name not in customers:
-        customers[name] = email
+        user = {name: email}
+        customers.update(user)
     else:
         print("That user already exists")
+    save(customers)
 
 
 def change(customers):
@@ -77,8 +80,10 @@ def change(customers):
     if name in customers:
         email = input("Please enter the new email address: ")
         customers[name] = email
+        customers.update()
     else:
         print('That user does not exist')
+    save(customers)
 
 
 def delete(customers):
@@ -87,13 +92,15 @@ def delete(customers):
         del customers[name]
     else:
         print("The customer was not found")
+    save(customers)
 
 
 def save(customers):
+    print(customers)
     print("The data file has been updated with your changes. ")
     save_file = open('customer_file.dat', 'wb')
     pickle.dump(customers, save_file)
     save_file.close()
-
+    menu()
 
 main()
