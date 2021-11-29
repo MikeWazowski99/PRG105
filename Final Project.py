d834398ad68fcaf944879051a92a9429304f5e81
Michael Tapia <89033407+MikeWazowski99@users.noreply.github.com>
2021-11-29 13:59:45 -0600

    Add files via upload

--- Final Project.py
+++ Final Project.py
@@ -9,65 +9,86 @@ List of things need to add: * a fun fact, * how much it cost to live there, * in
                             * weather in the country, * currency converter, * image of the country / specific area
 """
 
-import tkinter
+import tkinter as tk
 from tkinter import *
-# from PIL import ImageTk, Image
+from PIL import Image, ImageTk
 # from PIL import Image
 
 
+# class MyGUI:
+#     def __init__(self):
+#         # These self mains are there to add a popup for countries / states except for self main.window
+#         self.main_window = tkinter.Tk()
+#         self.main_US_value = tkinter.StringVar()
+#         self.main_France = tkinter.StringVar()
+#         self.main_Japan_value = tkinter.StringVar()
+#         self.main_Italy = tkinter.StringVar()
+#         self.main_Canada = tkinter.StringVar()
+#         self.main_Hawaii_value = tkinter.StringVar()
+#
+#         # 2 Frames
+#         self.info_frame = tkinter.Frame(self.main_window)
+#         self.button_frame = tkinter.Frame(self.main_window)
+#
+#         self.main_US = tkinter.Label(self.info_frame, textvariable=self.main_US_value)
+#         self.main_Japan = tkinter.Label(self.info_frame, textvariable=self.main_Japan_value)
+#         self.main_Hawaii = tkinter.Label(self.info_frame, textvariable=self.main_Hawaii_value)
+#
+#         # pack stuff
+#         self.main_US.pack()
+#         self.main_Japan.pack()
+#         self.main_Hawaii.pack()
+#
+#         # buttons
+#         self.show_info_button = tkinter.Button(self.button_frame, text='Show Info', command=self.show)
+#         self.quit_button = tkinter.Button(self.button_frame, text='Quit', command=self.main_window.destroy)
+#
+#         # pack buttons
+#         self.show_info_button.pack(side='left')
+#         self.quit_button.pack(side='right')
+#
+#         # frame pack
+#         self.info_frame.pack()
+#         self.button_frame.pack()
+#
+#         tkinter.mainloop()
+#
+#     def show(self):
+#         self.main_US_value.set('US')
+#         self.main_Japan_value.set('Japan')
+#         self.main_Hawaii_value.set('Hawaii')
+#
+#
+# my_gui = MyGUI()
+#
 
-
-class MyGUI:
-    def __init__(self):
-        # These self mains are there to add a popup for countries / states except for self main.window
-        self.main_window = tkinter.Tk()
-        self.main_US_value = tkinter.StringVar()
-        self.main_France = tkinter.StringVar()
-        self.main_Japan_value = tkinter.StringVar()
-        self.main_Italy = tkinter.StringVar()
-        self.main_Canada = tkinter.StringVar()
-        self.main_Hawaii_value = tkinter.StringVar()
-
-        # 2 Frames
-        self.info_frame = tkinter.Frame(self.main_window)
-        self.button_frame = tkinter.Frame(self.main_window)
-
-        self.main_US = tkinter.Label(self.info_frame, textvariable=self.main_US_value)
-        self.main_Japan = tkinter.Label(self.info_frame, textvariable=self.main_Japan_value)
-        self.main_Hawaii = tkinter.Label(self.info_frame, textvariable=self.main_Hawaii_value)
-
-        # pack stuff
-        self.main_US.pack()
-        self.main_Japan.pack()
-        self.main_Hawaii.pack()
-
-        # buttons
-        self.show_info_button = tkinter.Button(self.button_frame, text='Show Info', command=self.show)
-        self.quit_button = tkinter.Button(self.button_frame, text='Quit', command=self.main_window.destroy)
-
-        # pack buttons
-        self.show_info_button.pack(side='left')
-        self.quit_button.pack(side='right')
-
-        # frame pack
-        self.info_frame.pack()
-        self.button_frame.pack()
-
-        tkinter.mainloop()
-
-    def show(self):
-        self.main_US_value.set('US')
-        self.main_Japan_value.set('Japan')
-        self.main_Hawaii_value.set('Hawaii')
-
-
-my_gui = MyGUI()
+def on_click(event=None):
+    print("Image clicked")
 
 
 def main():
-    root = Tk()
+    # Icon for program
+    root = tk.Tk()
     root.title("World Explorer")
     root.iconbitmap("/Users/mtapia19/Desktop/PRG105/favicon-3.ico")
+    # Clickable images
+    image = ImageTk.open("tokyo.jpg")
+    photo = ImageTk.PhotoImage(image)
+    # label images
+    label = tk.Label(root, image=photo)
+    label.pack()
+    # binding stuff to image
+    label.bind('<Button-1>', 'on-click')
+    # button with image binded to the same function
+    b = tk.Button(root, image=photo, command=on_click)
+    b.pack()
+
+    # button with text closing window
+    b = tk.Button(root, text="Close", command=root.destroy)
+    b.pack()
+
+    # "start the engine"
     root.mainloop()
+
 
 main()
